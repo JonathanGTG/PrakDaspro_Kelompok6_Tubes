a85d1b9f9b3f065fc58954d2c24e66153525dd59
grethasbrn <siburiangrethas@gmail.com>
2025-12-02 13:27:37 +0700

    commit

---
+++ Fitur/exit.h
@@ -0,0 +1,9 @@
+#include "Task.h"
+
+void keluarprogram(){
+    system("cls");
+    printf(GREEN "===============================================\n" RESET);
+    printf(GREEN "    Terimakasih sudah menggunakan program\n"         RESET);
+    printf(GREEN "                TO DO LIST kami!\n"                      RESET);
+    printf(GREEN "===============================================\n" RESET);
+}
