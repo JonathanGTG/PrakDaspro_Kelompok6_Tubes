02661e21804ed572a730a008849f0a88b917bfef
Syalikhafaradhiya Agira <syalikhafa@gmail.com>
2025-12-02 13:30:13 +0700

    commit

---
+++ Fitur/checkstatus.h
@@ -0,0 +1,25 @@
+#include "Task.h"
+
+int adaTugasDone() {
+    FILE *file = fopen(FILE_TUGAS, "r");
+    Tugas tugas;
+
+    if (!file) return 0;
+
+    while (fscanf(file, "%29s %14s %9s",
+                  tugas.matkul,
+                  tugas.deadline,
+                  tugas.status) != EOF)
+    {
+        // check status DONE (bebas kapital)
+        if (strcasecmp(tugas.status, "Done") == 0 ||
+            strcasecmp(tugas.status, "Selesai") == 0)
+        {
+            fclose(file);
+            return 1; // Ada tugas done
+        }
+    }
+
+    fclose(file);
+    return 0; // Tidak ada yang done
+}
